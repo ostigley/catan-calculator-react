21b02f4d667a1cc29c668e797b754c69e542b888
Oliver Stigley <oliver@boost.co.nz>
2017-10-08 14:56:25 +1300

    don't render a node if there isn't one

--- src/Hex/HexWithNodes.jsx
+++ src/Hex/HexWithNodes.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Node from './Node.jsx'
 export default ({hexId, nodes}) => {
-  const nodesToRender = nodes.map( (nodeName,i) => (<Node
+  const nodesToRender = nodes.map( (nodeName,i) => ( nodeName ? <Node
                                                 nodeId={nodeName}
                                                 position={i}
-                                                key={`node_id_${nodeName}`}/>));
+                                                key={`node_id_${nodeName}`}/> : null));
   return (
     <div>
       <div className="hexagon" id={hexId}>
