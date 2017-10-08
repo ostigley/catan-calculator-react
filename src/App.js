ab951c7ae088b9246be1c77b55f338d61f227732
Oliver Stigley <oliver@boost.co.nz>
2017-10-09 08:19:12 +1300

    render with for loop

--- src/App.js
+++ src/App.js
@@ -4,11 +4,23 @@ import {hexesRowsNodes} from './hexesRowsNodes.js'
 import './App.css';
 
 class App extends Component {
-  render() {
+  constructor() {
+    super()
+    this.state = hexesRowsNodes
+  }
 
+  onChange(row, hexId, value) {
+    this.setState()
+  }
+  render() {
+    const rows = []
+    for(let i = 0; i < 5 ;i++) {
+      rows.push(<Row rowNum={i+1} key={`row_${i+1}`} hexes={this.state.rows[i]}/>)
+    }
     return (
       <div className="catan-container">
-        {hexesRowsNodes.rows.map((row, i) => <Row rowNum={i+1} key={`row_${i+1}`} hexes={row}/>)}
+      { rows }
+
       </div>);
   }
 }
