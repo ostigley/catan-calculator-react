0bc13cdbc91e57f8d2c342c203e19fb0bfbff2e3
Oliver Stigley <oliver@boost.co.nz>
2017-10-11 08:20:13 +1300

    nodes change colour depending score

--- src/Hex/Node.jsx
+++ src/Hex/Node.jsx
@@ -1,18 +1,32 @@
 import React from 'react';
 
 export default ({nodeId, position, hexValues}) => {
+  const stats = {
+    '0': 0,
+    '2': 1,
+    '3': 2,
+    '4': 3,
+    '5': 4,
+    '6': 5,
+    '8': 5,
+    '9': 4,
+    '10': 3,
+    '11': 2,
+    '12': 1
+  }
   const parentHexes = nodeId.split('')
-  const diceRollPropbability = hexValues.reduce( (total, hex) => {
+  const nodeScore = hexValues.reduce( (total, hex) => {
+
   if (parentHexes.includes(String(hex.hexId))) {
-      return Number(hex.value) + total
+      return stats[String(hex.value)] + total
     } else {
       return total
     }
   }, 0);
 
-  console.log(diceRollPropbability)
   return (
-    <div className={`node node--${position}`} id={nodeId}>
+    <div className={`node node--${position} node--probability-${nodeScore}`} id={nodeId}>
+      <p>{nodeScore}</p>
     </div>);
 };
 
--- src/hexesRowsNodes.js
+++ src/hexesRowsNodes.js
@@ -1,32 +1,32 @@
 const hexesRowsNodes = {
   '1': [
-    {'hexId': 's', 'row': '1', 'nodes': ['s', 'sh', 'shb', 'srb','sr', 's'], 'value': '1' },
-    {'hexId': 'h', 'row': '1', 'nodes': ['h', 'hi'], 'value': '' },
-    {'hexId': 'i', 'row': '1', 'nodes': ['i', 'i', 'ij', 'icj', 'ich'], 'value': '' },
+    {'hexId': 's', 'row': '1', 'nodes': ['s', 'sh', 'shb', 'srb','sr', 's'], 'value': '0' },
+    {'hexId': 'h', 'row': '1', 'nodes': ['h', 'hi'], 'value': '0' },
+    {'hexId': 'i', 'row': '1', 'nodes': ['i', 'i', 'ij', 'icj', 'ich'], 'value': '0' },
   ],
   '2': [
-    {'hexId': 'r', 'row': '2', 'nodes': [undefined, undefined, 'rbg', 'rgq', 'rq', 'r'], 'value': '' },
-    {'hexId': 'b', 'row': '2', 'nodes': [undefined, 'bch', 'bca', 'bag'], 'value': '' },
-    {'hexId': 'c', 'row': '2', 'nodes': [undefined, undefined, 'cjd', 'cad'], 'value': '' },
-    {'hexId': 'j', 'row': '2', 'nodes': [undefined, 'j', 'j', 'jdk'], 'value': '' }
+    {'hexId': 'r', 'row': '2', 'nodes': [undefined, undefined, 'rbg', 'rgq', 'rq', 'r'], 'value': '0' },
+    {'hexId': 'b', 'row': '2', 'nodes': [undefined, 'bch', 'bca', 'bag'], 'value': '0' },
+    {'hexId': 'c', 'row': '2', 'nodes': [undefined, undefined, 'cjd', 'cad'], 'value': '0' },
+    {'hexId': 'j', 'row': '2', 'nodes': [undefined, 'j', 'j', 'jdk'], 'value': '0' }
   ],
   '3': [
-    {'hexId': 'q', 'row': '3', 'nodes': [undefined, undefined, 'qgp', 'qp', 'q', 'q'], 'value': '' },
-    {'hexId': 'g', 'row': '3', 'nodes': [undefined, undefined, 'gaf', 'gfp'], 'value': '' },
-    {'hexId': 'a', 'row': '3', 'nodes': [undefined, undefined, 'ade', 'aef'], 'value': '' },
-    {'hexId': 'd', 'row': '3', 'nodes': [undefined, undefined, 'dkl', 'dje'], 'value': '' },
-    {'hexId': 'k', 'row': '3', 'nodes': [undefined, 'k', 'k', 'kl'], 'value': '' },
+    {'hexId': 'q', 'row': '3', 'nodes': [undefined, undefined, 'qgp', 'qp', 'q', 'q'], 'value': '0' },
+    {'hexId': 'g', 'row': '3', 'nodes': [undefined, undefined, 'gaf', 'gfp'], 'value': '0' },
+    {'hexId': 'a', 'row': '3', 'nodes': [undefined, undefined, 'ade', 'aef'], 'value': '0' },
+    {'hexId': 'd', 'row': '3', 'nodes': [undefined, undefined, 'dkl', 'dje'], 'value': '0' },
+    {'hexId': 'k', 'row': '3', 'nodes': [undefined, 'k', 'k', 'kl'], 'value': '0' },
   ],
   '4': [
-    {'hexId': 'p', 'row': '4', 'nodes': [undefined, undefined, 'pfo', 'po', 'p'], 'value': '' },
-    {'hexId': 'f', 'row': '4', 'nodes': [undefined, undefined, 'fen', 'fno'], 'value': '' },
-    {'hexId': 'e', 'row': '4', 'nodes': [undefined, undefined, 'elm', 'emn'], 'value': '' },
-    {'hexId': 'l', 'row': '4', 'nodes': [undefined, undefined, 'l', 'lme'], 'value': '' }
+    {'hexId': 'p', 'row': '4', 'nodes': [undefined, undefined, 'pfo', 'po', 'p'], 'value': '0' },
+    {'hexId': 'f', 'row': '4', 'nodes': [undefined, undefined, 'fen', 'fno'], 'value': '0' },
+    {'hexId': 'e', 'row': '4', 'nodes': [undefined, undefined, 'elm', 'emn'], 'value': '0' },
+    {'hexId': 'l', 'row': '4', 'nodes': [undefined, undefined, 'l', 'lme'], 'value': '0' }
   ],
   '5': [
-    {'hexId': 'o', 'row': '5', 'nodes': [undefined, undefined, 'on', 'o', 'o'], 'value': '' },
-    {'hexId': 'n', 'row': '5', 'nodes': [undefined, undefined, 'nm', 'n'], 'value': '' },
-    {'hexId': 'm', 'row': '5', 'nodes': [undefined, undefined, 'm', 'm'], 'value': '' },
+    {'hexId': 'o', 'row': '5', 'nodes': [undefined, undefined, 'on', 'o', 'o'], 'value': '0' },
+    {'hexId': 'n', 'row': '5', 'nodes': [undefined, undefined, 'nm', 'n'], 'value': '0' },
+    {'hexId': 'm', 'row': '5', 'nodes': [undefined, undefined, 'm', 'm'], 'value': '0' },
   ]
 };
 
