44fd27befd9413d5716f9ba5c5aaa27618b37099
Oliver Stigley <oliver@boost.co.nz>
2017-10-11 08:38:45 +1300

    traditional starting point values
    https://i2.wp.com/www.catanmaps.com/wp-content/uploads/2015/06/Tournamen
    t-Board-1-1.jpg

--- src/hexesRowsNodes.js
+++ src/hexesRowsNodes.js
@@ -1,32 +1,32 @@
 const hexesRowsNodes = {
   '1': [
-    {'hexId': 's', 'row': '1', 'nodes': ['s', 'sh', 'shb', 'srb','sr', 's'], 'value': '0' },
-    {'hexId': 'h', 'row': '1', 'nodes': ['h', 'hi'], 'value': '0' },
-    {'hexId': 'i', 'row': '1', 'nodes': ['i', 'i', 'ij', 'icj', 'ich'], 'value': '0' },
+    {'hexId': 's', 'row': '1', 'nodes': ['s', 'sh', 'shb', 'srb','sr', 's'], 'value': '8' },
+    {'hexId': 'h', 'row': '1', 'nodes': ['h', 'hi'], 'value': '4' },
+    {'hexId': 'i', 'row': '1', 'nodes': ['i', 'i', 'ij', 'icj', 'ich'], 'value': '6' },
   ],
   '2': [
-    {'hexId': 'r', 'row': '2', 'nodes': [undefined, undefined, 'rbg', 'rgq', 'rq', 'r'], 'value': '0' },
-    {'hexId': 'b', 'row': '2', 'nodes': [undefined, 'bch', 'bca', 'bag'], 'value': '0' },
-    {'hexId': 'c', 'row': '2', 'nodes': [undefined, undefined, 'cjd', 'cad'], 'value': '0' },
-    {'hexId': 'j', 'row': '2', 'nodes': [undefined, 'j', 'j', 'jdk'], 'value': '0' }
+    {'hexId': 'r', 'row': '2', 'nodes': [undefined, undefined, 'rbg', 'rgq', 'rq', 'r'], 'value': '11' },
+    {'hexId': 'b', 'row': '2', 'nodes': [undefined, 'bch', 'bca', 'bag'], 'value': '2' },
+    {'hexId': 'c', 'row': '2', 'nodes': [undefined, undefined, 'cjd', 'cad'], 'value': '12' },
+    {'hexId': 'j', 'row': '2', 'nodes': [undefined, 'j', 'j', 'jdk'], 'value': '10' }
   ],
   '3': [
-    {'hexId': 'q', 'row': '3', 'nodes': [undefined, undefined, 'qgp', 'qp', 'q', 'q'], 'value': '0' },
-    {'hexId': 'g', 'row': '3', 'nodes': [undefined, undefined, 'gaf', 'gfp'], 'value': '0' },
+    {'hexId': 'q', 'row': '3', 'nodes': [undefined, undefined, 'qgp', 'qp', 'q', 'q'], 'value': '5' },
+    {'hexId': 'g', 'row': '3', 'nodes': [undefined, undefined, 'gaf', 'gfp'], 'value': '9' },
     {'hexId': 'a', 'row': '3', 'nodes': [undefined, undefined, 'ade', 'aef'], 'value': '0' },
-    {'hexId': 'd', 'row': '3', 'nodes': [undefined, undefined, 'dkl', 'dje'], 'value': '0' },
-    {'hexId': 'k', 'row': '3', 'nodes': [undefined, 'k', 'k', 'kl'], 'value': '0' },
+    {'hexId': 'd', 'row': '3', 'nodes': [undefined, undefined, 'dkl', 'dje'], 'value': '11' },
+    {'hexId': 'k', 'row': '3', 'nodes': [undefined, 'k', 'k', 'kl'], 'value': '9' },
   ],
   '4': [
-    {'hexId': 'p', 'row': '4', 'nodes': [undefined, undefined, 'pfo', 'po', 'p'], 'value': '0' },
-    {'hexId': 'f', 'row': '4', 'nodes': [undefined, undefined, 'fen', 'fno'], 'value': '0' },
-    {'hexId': 'e', 'row': '4', 'nodes': [undefined, undefined, 'elm', 'emn'], 'value': '0' },
-    {'hexId': 'l', 'row': '4', 'nodes': [undefined, undefined, 'l', 'lme'], 'value': '0' }
+    {'hexId': 'p', 'row': '4', 'nodes': [undefined, undefined, 'pfo', 'po', 'p'], 'value': '3' },
+    {'hexId': 'f', 'row': '4', 'nodes': [undefined, undefined, 'fen', 'fno'], 'value': '8' },
+    {'hexId': 'e', 'row': '4', 'nodes': [undefined, undefined, 'elm', 'emn'], 'value': '3' },
+    {'hexId': 'l', 'row': '4', 'nodes': [undefined, undefined, 'l', 'lme'], 'value': '6' }
   ],
   '5': [
-    {'hexId': 'o', 'row': '5', 'nodes': [undefined, undefined, 'on', 'o', 'o'], 'value': '0' },
-    {'hexId': 'n', 'row': '5', 'nodes': [undefined, undefined, 'nm', 'n'], 'value': '0' },
-    {'hexId': 'm', 'row': '5', 'nodes': [undefined, undefined, 'm', 'm'], 'value': '0' },
+    {'hexId': 'o', 'row': '5', 'nodes': [undefined, undefined, 'on', 'o', 'o'], 'value': '10' },
+    {'hexId': 'n', 'row': '5', 'nodes': [undefined, undefined, 'nm', 'n'], 'value': '4' },
+    {'hexId': 'm', 'row': '5', 'nodes': [undefined, undefined, 'm', 'm'], 'value': '5' },
   ]
 };
 
